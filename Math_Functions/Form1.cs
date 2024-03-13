dfa0790df801c2c87d3e917a438a2f335045a947
Sidarciftcioglu <Sidarciftcioglu47@gmail.com>
2024-03-13 08:45:02 +0300

    Add project files.

---
+++ Math_Functions/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Math_Functions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1=Convert.ToDouble(textBox1.Text);
+            //label2.Text =Convert.ToString( Math.Abs(n1));
+            //label2.Text = Math.Ceiling(n1).ToString();
+            //label2.Text = Math.Floor(n1).ToString();
+            //label2.Text = Math.Pow(n1,3).ToString();
+            label2.Text=Math.Sqrt(n1).ToString();
+        }
+    }
+}
